08a31ed6c0a6314de7c78d0f80238d485fb9522f
Vincent Kouoï <vincentkouoi@gmail.com>
2021-02-18 16:57:50 +0100

    Update env

--- front/src/App.js
+++ front/src/App.js
@@ -26,7 +26,7 @@ function App() {
       return;
     }
 
-    axios.get(`http://localhost:4000/verifyToken?token=${token}`).then(response => {
+    axios.get(`${process.env.REACT_APP_SERVER_URL}/verifyToken?token=${token}`).then(response => {
       setUserSession(response.data.token, response.data.user);
       setAuthLoading(false);
     }).catch(error => {
